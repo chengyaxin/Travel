c7157298fe261dae3d55d7031afec96fdaf1ee81
chengyaxin <1227965856@qq.com>
2018-08-20 11:34:38 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 //解决移动端点击延长300毫秒问题用fastclick
 import fastClick from 'fastclick'
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
+    }
   ]
 })
