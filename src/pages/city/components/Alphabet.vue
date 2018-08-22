c347cd7d017a6b61dc802d7f88b0294e3c39dd0b
chengyaxin <1227965856@qq.com>
2018-08-22 14:13:54 +0800

    add city list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="list">
+		<li class="item">A</li>
+		<li class="item">B</li>
+		<li class="item">C</li>
+		<li class="item">D</li>
+	</div>
+</template>
+<script>
+	export default {
+		name:"CityAlphabet",
+
+	}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+	display:flex
+	flex-direction:column
+	justify-content:center
+	position:absolute
+	right:0
+	top:1.58rem
+	bottom:0
+	width:.4rem
+	.item
+		text-align:center
+		line-height:.4rem
+		color:$bgColor
+		list-style:none
+</style>
