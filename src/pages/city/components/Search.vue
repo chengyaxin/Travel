1694a943f9bbe63420e4dec5159da6d043e97945
chengyaxin <1227965856@qq.com>
2018-08-23 15:19:26 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,11 +1,74 @@
 <template>
-	<div class="search">
-		<input class="search-input" type="text" placeholder="输入城市名或拼音" />
+	<div>
+		<div class="search">
+			<input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keyword"/>
+		</div>
+		<div class="search-content" ref="search" v-show="keyword">
+			<ul>
+				<li v-for="item of list" :key="item.id" class="search-item border-bottom">
+					{{item.name}}
+				</li>
+				<li class="search-item border-bottom" v-show="hasNoData">
+					没有找到匹配数据
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 	export default {
-		name:"CitySearch"
+		name:"CitySearch",
+		data()
+		{
+			return {
+				keyword:'',
+				list:[],
+				timer:null
+			}
+		},
+		props:{
+			cities:Object
+		},
+		computed:{
+			hasNoData()
+			{
+				return !this.list.length
+			}
+		},
+		watch:{
+			keyword()
+			{
+				//监听keyword的变化
+				if(this.timer)
+				{
+					clearTimeout(this.timer)
+				}
+				if(!this.keyword)
+				{
+					this.list = []
+					return 
+				}
+				this.time = setTimeout(()=>{
+					const result = []
+					for(let i in this.cities)
+					{
+						//循环每一个ABCD下的数组
+						this.cities[i].forEach((value)=>{
+							//value.spell是一个字符串，indexOf表示指定字符串在value.spell中首次出现的索引，若没找到则返回-1
+							if(value.spell.indexOf(this.keyword)>-1 || value.name.indexOf(this.keyword)>-1)
+							{
+								result.push(value)
+							}
+						})
+					}
+					this.list = result
+				},100)
+			}
+		},
+		mounted(){
+			this.scroll = new Bscroll(this.$refs.search)
+		}
 	}
 </script>
 <style lang="stylus" scoped>
@@ -23,4 +86,18 @@
 		text-align: center
 		border-radius: .06rem
 		color: #666
+.search-content
+	z-index:1
+	overflow:hidden
+	position:absolute
+	top:1.58rem
+	left:0
+	right:0
+	bottom:0
+	background:#eee
+	.search-item
+		line-height:.62rem
+		padding-left:.2rem
+		color:#666
+		background:#fff
 </style>
